19f0e2b9f1bbd9477d3efcc82349435fd268b87f
Muhammad Haziq Shahrin Al Johary <haziq.al-johary.18@ucl.ac.uk>
2021-03-10 16:34:01 +0000

    Added search routes by genre

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -12,4 +12,24 @@ router.get('/', async function(req, res, next){
     }
 });
 
+router.get('/filtered', async function(req, res, next){
+    try{
+        res.json(await movies.getFilteredMoviesByGenre(req.query.page, req.query.sortBy, req.query.orderBy, req.query.genre));
+    }
+    catch(err){
+        console.error(`Error while retrieving movies `, err.message);
+        next(err);
+    }
+});
+
+router.get('/multipleFiltered', async function(req, res, next){
+    try{
+        res.json(await movies.getMultipleFilteredMoviesByGenre(req.query.page, req.query.sortBy, req.query.orderBy, req.query.genre1, req.query.genre2));
+    }
+    catch(err){
+        console.error(`Error while retrieving movies `, err.message);
+        next(err);
+    }
+});
+
 module.exports = router;
--- src/services/movies.js
+++ src/services/movies.js
@@ -28,6 +28,59 @@ async function getMovies(page = 1, sortBy = 'movieId', orderBy = 'asc', filterBy
     }
 }
 
+async function getFilteredMoviesByGenre(page = 1, sortBy = 'movieId', orderBy = 'asc', genre = '%'){
+    const offset = helper.getOffset(page, config.listPerPage);
+    const movie_genre = helper.sanitiseParams(genre);
+    const sort = helper.sanitiseParams(sortBy);
+    const order = helper.sanitiseParams(orderBy);
+    var sql = format("SELECT * from movies WHERE movieId IN (SELECT movieId from movie_genre WHERE genreId IN (SELECT genreId from genres WHERE genre::text LIKE %L)) ORDER BY %s %s OFFSET %L LIMIT %L", 
+                    movie_genre, sort, order, offset, config.listPerPage)
+    const rows = await db.query(
+        sql,
+        []
+    );
+    const data = helper.emptyOrRows(rows)
+    const meta = {page,
+                  sortBy,
+                  orderBy,
+                  movie_genre
+                };
+    return{
+        data, 
+        meta
+    }
+}
+
+async function getMultipleFilteredMoviesByGenre(page = 1, sortBy = 'movieId', orderBy = 'asc', genre1 = '%', genre2 = '%'){
+    const offset = helper.getOffset(page, config.listPerPage);
+    const genre_1 = helper.sanitiseParams(genre1);
+    const genre_2 = helper.sanitiseParams(genre2);
+    const sort = helper.sanitiseParams(sortBy);
+    const order = helper.sanitiseParams(orderBy);
+    var sql = format("SELECT * from movies WHERE movieId IN (SELECT movieId from movie_genre WHERE genreId IN (SELECT genreId from genres WHERE genre::text LIKE %L)) " + 
+                    "INTERSECT SELECT * from movies WHERE movieId IN (SELECT movieId from movie_genre WHERE genreId IN (SELECT genreId from genres WHERE genre::text LIKE %L)) ORDER BY %s %s OFFSET %L LIMIT %L", 
+                    genre_1, genre_2, sort, order, offset, config.listPerPage)
+    const rows = await db.query(
+        sql,
+        []
+    );
+    const data = helper.emptyOrRows(rows)
+    const meta = {page,
+                  sortBy,
+                  orderBy,
+                  genre_1,
+                  genre_2
+                };
+    return{
+        data, 
+        meta
+    }
+}
+
+
+
 module.exports = {
-    getMovies
+    getMovies,
+    getFilteredMoviesByGenre,
+    getMultipleFilteredMoviesByGenre
 }
